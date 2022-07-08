86404988d0de22e3386ac0428a90927c2be1b3a2
Mumuni Tahiru <peptahiru@gmail.com>
2022-07-08 06:47:24 +0000

    commit

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Calculator {
     public static void main(String[] args) {
         // declaring variables
@@ -15,14 +17,29 @@ public class Calculator {
         principle = input.nextDouble();
         System.out.println("Enter the annual interest rate (in percentage): ");
         rate = input.nextDouble();
-        System.out.println("Enter the period (in months): ");
+        System.out.println("Enter the period (in years): ");
         period = input.nextInt();
         // performing necessary calculations
         // to get the monthly interest rate
-        int monthlyIntRate;
+        double monthlyIntRate;
         monthlyIntRate = (rate / 100) / 12;
         period *= 12;
         mortgage = (principle * monthlyIntRate * Math.sqrt(1 + monthlyIntRate)) / Math.sqrt(1 + monthlyIntRate) - 1;
+        // display the results using a dialog box
+        /*
+         * JOptionPane.showMessageDialog(null, "The principle entered is: " +
+         * principle);
+         * JOptionPane.showMessageDialog(null, "The period entered in months is: " +
+         * period);
+         * JOptionPane.showMessageDialog(null, "Your monthly pay rate is : " +
+         * monthlyIntRate);
+         * JOptionPane.showMessageDialog(null, "Your mortgage for the period of " +
+         * period + "is: " + mortgage);
+         */
 
+        System.out.println("The principle entered is: " + principle);
+        System.out.println("The period entered in months is: " + period);
+        System.out.println("Your monthly pay rate is : " + monthlyIntRate);
+        System.out.println("your mortgage for this period is: " + mortgage);
     }
 }
